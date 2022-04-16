4c29ccd403f9403f0431eb514c6448a89801fd85
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-04-17 00:18:13 +0600

    navbar added without responsivness

--- src/Components/Page/Shared/Navbar/Navbar.js
+++ src/Components/Page/Shared/Navbar/Navbar.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between mx-40'>
-            <h1>Medicure</h1>
-            <div>
-                <Link to="home">Home</Link>
-                <Link to="/blogs">Blogs</Link>
-                <Link to="/about">About</Link>
-                <Link to="/login">Log in</Link>
+        <div className='flex justify-between items-center mx-40'>
+            <h1 className='text-4xl font-bold'>Medi<span className='text-indigo-700'>cure</span></h1>
+            <div className='w-1/2 flex justify-evenly text-indigo-700 text-xl font-semibold p-7'>
+                <Link className='hover:bg-indigo-300 hover:text-black px-3 py-1 hover:scale-105 rounded transition-all' to="home">Home</Link>
+                <Link className='hover:bg-indigo-300 hover:text-black px-3 py-1 hover:scale-105 rounded transition-all' to="/blogs">Blogs</Link>
+                <Link className='hover:bg-indigo-300 hover:text-black px-3 py-1 hover:scale-105 rounded transition-all' to="/about">About</Link>
+                <Link className='hover:bg-indigo-300 hover:text-black px-3 py-1 hover:scale-105 rounded transition-all' to="/login">Log in</Link>
             </div>
         </div>
     );
