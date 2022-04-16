dff294831c6793b41bdc0d3841d66d13c514b5f8
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-04-16 23:21:44 +0600

    project setup completed

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBGy2IKL0OWdZhZ-hfZff-8UFBjtm4m96I",
+    authDomain: "medicure-2fe04.firebaseapp.com",
+    projectId: "medicure-2fe04",
+    storageBucket: "medicure-2fe04.appspot.com",
+    messagingSenderId: "49865845849",
+    appId: "1:49865845849:web:0bc8f5ff166dff00acd548"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
