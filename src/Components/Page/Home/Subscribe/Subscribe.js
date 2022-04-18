81862990253bb27dc18ed9788cdb18079fd8bfe5
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-04-18 23:45:24 +0600

    simple problem fixed

--- src/Components/Page/Home/Subscribe/Subscribe.js
+++ src/Components/Page/Home/Subscribe/Subscribe.js
@@ -8,7 +8,7 @@ const Subscribe = () => {
                 <h1 className='text-2xl md:text-4xl font-bold text-indigo-700'>NEED A PERSONAL HEALTH PALN?</h1>
                 <h4 className='text-center text-3xl text-indigo-900 font-semibold my-10'>Subscribe Now!</h4>
 
-                <input className='w-2/1 md:w-2/3 bg-indigo-300 placeholder-black outline-none font-semibold px-5 py-3 rounded-l-xl' type="email" placeholder='Enter Your Email' required/>
+                <input className='w-1/2 md:w-2/3 bg-indigo-300 placeholder-black outline-none font-semibold px-5 py-3 rounded-l-xl' type="email" placeholder='Enter Your Email' required />
 
                 <button type='submit' className='bg-indigo-500 font-semibold px-5 py-3 rounded-r-xl '>Subscribe</button>
             </div>
